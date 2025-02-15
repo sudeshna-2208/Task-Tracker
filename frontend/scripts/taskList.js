6ad8fe3a3bc057efbf6b71b1dbf2d8994e200568
sudeshna-2208 <sudeshnasamanta260@gmail.com>
2025-02-15 13:43:39 +0530

    Update taskList.js

--- frontend/scripts/taskList.js
+++ frontend/scripts/taskList.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let currentTaskId = null; // Track the task being edited
 
-    const baseURL = "https://task-tracker-backend-sjkf.onrender.com"; // Deployed backend URL
+    const baseURL = "https://task-tracker-riyl.onrender.com"; // Deployed backend URL
 
     // Fetch tasks based on priority filter
     const fetchTasks = async (priority = 'all') => {
